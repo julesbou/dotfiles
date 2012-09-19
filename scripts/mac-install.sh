9b33737c8e6d220986b57337f5269a8f700b6a2a
Jules Boussekeyt <jules.boussekeyt@gmail.com>
2012-09-19 10:10:31 +0200

    [mac-install.sh] disable dashboard

--- scripts/mac-install.sh
+++ scripts/mac-install.sh
@@ -1,4 +1,7 @@
 echo 'Save screenshots to Pictures folder';
 defaults write com.apple.screencapture location "/Users/$(whoami)/Pictures/screenshot"
 
+echo 'disable dashboard';
+defaults write com.apple.dashboard mcx-disabled -boolean YES
+
 echo '\r\n/!\ You can now run this script: https://gist.github.com/2260182\r\n';
