d2de9f4ab91cc0a007398cb35390bbf83d7b6b36
Jules Boussekeyt <jules.boussekeyt@gmail.com>
2012-09-20 10:07:59 +0200

    increase 'ulimit -n' in mac-install.sh

--- scripts/mac-install.sh
+++ scripts/mac-install.sh
@@ -4,4 +4,7 @@ defaults write com.apple.screencapture location "/Users/$(whoami)/Pictures/scree
 echo 'disable dashboard';
 defaults write com.apple.dashboard mcx-disabled -boolean YES
 
+echo 'set "ulimit -n" to 1024 (default is 256)';
+launchctl limit maxfiles 1024 1024
+
 echo '\r\n/!\ You can now run this script: https://gist.github.com/2260182\r\n';
