0dd6c85073f80f7d606f1057c28162a495f5bb0f
Jules <jules.boussekeyt@gmail.com>
2014-01-14 22:39:47 +0100

    re enable notification center as it's not working

--- scripts/mac-install.sh
+++ scripts/mac-install.sh
@@ -1,10 +1,6 @@
 echo 'Save screenshots to Pictures folder';
 defaults write com.apple.screencapture location "/Users/$(whoami)/Destkop"
 
-echo 'disable notification center';
-sudo defaults write /System/Library/LaunchAgents/com.apple.notificationcenterui KeepAlive -bool false
-killall NotificationCenter
-
 echo 'disable dashboard';
 defaults write com.apple.dashboard mcx-disabled -boolean YES
 
