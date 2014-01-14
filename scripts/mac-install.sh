e3945922cdbdbd5dfabd6d0a84ff02793cacf97b
Jules <jules.boussekeyt@gmail.com>
2014-01-14 22:28:46 +0100

    disable notification center

--- scripts/mac-install.sh
+++ scripts/mac-install.sh
@@ -1,6 +1,10 @@
 echo 'Save screenshots to Pictures folder';
 defaults write com.apple.screencapture location "/Users/$(whoami)/Destkop"
 
+echo 'disable notification center';
+sudo defaults write /System/Library/LaunchAgents/com.apple.notificationcenterui KeepAlive -bool false
+killall NotificationCenter
+
 echo 'disable dashboard';
 defaults write com.apple.dashboard mcx-disabled -boolean YES
 
