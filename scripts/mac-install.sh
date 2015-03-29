080447643f9006e7a032eef8440590c0b1344bc2
Jules Boussekeyt <jules.boussekeyt@gmail.com>
2015-03-29 19:23:49 +0200

    move to yadr

--- scripts/mac-install.sh
+++
@@ -1,7 +0,0 @@
-echo 'Save screenshots to Pictures folder';
-defaults write com.apple.screencapture location "/Users/$(whoami)/Destkop"
-
-echo 'disable dashboard';
-defaults write com.apple.dashboard mcx-disabled -boolean YES
-
-echo '\r\n/!\ You can now run this script: https://gist.github.com/2260182\r\n';
