526e7b03dd664e365b7e591ba0bedf32039dcb16
Jules B <jules.bous@gmail.com>
2016-10-10 21:23:25 +0200

    update vim version

--- .zsh.after/alias.zsh
+++ .zsh.after/alias.zsh
@@ -3,7 +3,7 @@ alias vi="vim"
 # dirty override of default vim because
 # - we can't remove/update default vim
 # - we dont want to alter PATH order for that
-alias vim='/usr/local/Cellar/vim/7.4.1525/bin/vim'
+alias vim='/usr/local/Cellar/vim/7.4.488/bin/vim'
 
 alias nlog='tail -f /usr/local/etc/nginx/logs/*'
 
